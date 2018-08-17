04c19437e07dd3f4acd2749e344df95b5890d861
David Mzareulyan <david@hiero.ru>
2018-08-17 23:03:03 +0300

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import CleanWebpackPlugin from 'clean-webpack-plugin';
+import { Configuration } from 'webpack';
+
+const commonConfig: Configuration = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+
+  externals: {},
+  plugins: [new CleanWebpackPlugin(['dist'])],
+};
+
+const devConfig: Configuration = {
+  ...commonConfig,
+  devtool: 'source-map',
+  mode: 'development',
+};
+
+const prodConfig: Configuration = {
+  ...commonConfig,
+  mode: 'production',
+};
+
+export default (process.env.NODE_ENV === 'production' ? prodConfig : devConfig);

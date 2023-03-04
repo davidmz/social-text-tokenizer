fbf98046d407b4077712d7e8e1654477bccd8c1c
David Mzareulyan <david@hiero.ru>
2023-03-04 15:33:57 +0300

    Export helper functions as ./lib export

---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export { default as byRegexp, makeToken, type MatchProcessor } from './byRegexp';
+export { default as byWords } from './byWords';
+export { default as CharRanges, type SomeTuples, type TupleArg } from './CharRanges';
+export { spacesAndPunctuation, wordAdjacentChars } from './chars';

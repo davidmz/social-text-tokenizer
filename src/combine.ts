9f0aa6094a82fd1ee89d1e69a51b471aa208a0dc
David Mzareulyan <david@hiero.ru>
2023-10-05 18:32:37 +0300

    Clean up 'combine' logic

--- src/combine.ts
+++ src/combine.ts
@@ -1,4 +1,4 @@
-import type { Tokenizer } from './types';
+import type { Token, Tokenizer } from './types';
 
 /**
  * Combine combines several tokenizers into one. It leaves only non-overlapping
@@ -16,22 +16,21 @@ export function combine(...tokenizers: Tokenizer[]): Tokenizer {
       return allTokens;
     }
 
-    let last = allTokens.shift()!;
-    const result = [last];
+    let last: Token | null = null;
+    const result: Token[] = [];
     for (const token of allTokens) {
-      // If token starts at the same offset, but has longer text, use it instead
-      // of last token
-      if (
+      // If token starts after (or at) the last token end, append it
+      if (!last || token.offset >= last.offset + last.text.length) {
+        last = token;
+        result.push(token);
+        // If token starts at the same offset, but has longer text, use it
+        // instead of last token
+      } else if (
         token.offset === last.offset &&
         token.text.length > last.text.length
       ) {
         last = token;
         result.splice(-1, 1, token);
-      }
-      // If token starts after (or at) the last token end, append it
-      else if (token.offset >= last.offset + last.text.length) {
-        last = token;
-        result.push(token);
       }
       // Otherwise just skip this token (starts inside the last one, or has
       // shorter text)

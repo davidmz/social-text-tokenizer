0aacd62ea7791eabacc769d88722228ffe47804a
David Mzareulyan <david@hiero.ru>
2024-10-30 13:46:46 +0300

    Handle arrow in the square or round brackets

--- src/tokens/arrows.ts
+++ src/tokens/arrows.ts
@@ -5,7 +5,7 @@ import { Characters, makeToken, reTokenizer } from '../utils';
 
 const defaultRe = /\u2191+|\^(?:[1-9]\d*|\^*)/g;
 
-const validNearChars = new Characters(
+const validAdjacentChars = new Characters(
   // \s-like characters
   0x0009,
   0x0020,
@@ -14,7 +14,12 @@ const validNearChars = new Characters(
   0x000d,
   // Also spaces
   [0x2000, 0x200a],
-).withChars('.,');
+)
+  // Dot and comma
+  .withChars('.,');
+
+const openingBrackets = new Characters('[(');
+const closingBrackets = new Characters(')]');
 
 export function arrows(re = defaultRe) {
   return withFilters(reTokenizer(re, makeToken(ARROWS)), withNearChars);
@@ -23,7 +28,7 @@ export function arrows(re = defaultRe) {
 function withNearChars(token: Token, input: string) {
   if (
     token.text.charAt(0) === '\u2191' ||
-    // Two or more ^
+    // Two or more arrows
     (token.text.length > 1 &&
       token.text === token.text.charAt(0).repeat(token.text.length)) ||
     // At start of text
@@ -36,10 +41,23 @@ function withNearChars(token: Token, input: string) {
   const nextChar = input.charAt(token.offset + token.text.length);
 
   if (
+    // Token is inside braces
+    openingBrackets.includesChar(prevChar) &&
+    closingBrackets.includesChar(nextChar)
+  ) {
+    return token;
+  }
+
+  if (
+    // ^W or ^H, the common "forget the previous word" shortcuts
     /[WH]/.test(nextChar) ||
+    // Probably a number with an exponent part like 10^6
     /\d/.test(prevChar) ||
-    (!validNearChars.includesChar(prevChar) &&
-      !validNearChars.includesChar(nextChar))
+    // Don't has a valid adjacent char from at least one side
+    !(
+      validAdjacentChars.includesChar(prevChar) ||
+      validAdjacentChars.includesChar(nextChar)
+    )
   ) {
     return null;
   }
